8c45ec9fcc91eb8123fa0c78eeb2126a7784bc27
victor-medeiros <victor.msilva@bandtec.com.br>
2020-11-19 19:36:03 -0300

    parada daora

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const msg = "runing!";
+
+app.use(cors());
+
+app.get("/api", (_, res)=>
+res.json({
+    data: msg,
+})
+);
+
+app.listen(process.env.PORT || 3333, ()=> console.log(msg));
